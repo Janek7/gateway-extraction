02e2e285337383dc340bb614436cc5488f7eb614
Janek Putz <janekputz@web.de>
2022-10-26 15:36:55 +0200

    output goldstandards to formatted and filtered jsons

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import json
 import os
 import pickle
 
+from petreader.labels import *
 
 def format_json_file(filename: str, indent: int = 4) -> None:
     """
@@ -17,7 +18,7 @@ def format_json_file(filename: str, indent: int = 4) -> None:
         json.dump(result_data, file, indent=indent)
 
 
-def save_as_pickle(obj, filename):
+def save_as_pickle(obj: object, filename: str) -> None:
     """
     save an object in a pickle file dump
     :param obj: object to dump
@@ -32,7 +33,7 @@ def save_as_pickle(obj, filename):
         pickle.dump(obj, file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def load_pickle(filename):
+def load_pickle(filename: str) -> object:
     """
     load an object from a given pickle file
     :param filename: source file
@@ -40,3 +41,35 @@ def load_pickle(filename):
     """
     with open(os.path.abspath(filename), 'rb') as file:
         return pickle.load(file)
+
+
+def goldstandards_to_json(objects: str = 'relevant') -> None:
+    """
+    read pickled gold standard objects and output as formatted (and filtered on objects relevant for thesis) json
+    :param objects: which entities to include in output, values:
+                    'relevant' -> only 'flow' and 'same gateway' for relations
+                                  only 'activity', 'xor gateway', 'and gateway' and 'condition specification' for tokens
+                    'all' -> all unfiltered
+    :return:
+    """
+    def filter_goldstandard_dict(goldstandard_dict, target_object_keys):
+        return {doc: {token_type: token_list for token_type, token_list in doc_dict.items()
+                      if token_type.lower() in [k.lower() for k in target_object_keys]}
+                for doc, doc_dict in goldstandard_dict.items()}
+
+    relations_goldstandard = load_pickle("data/other/relations_goldstandard.pkl")
+    if objects == 'relevant':
+        relations_goldstandard = filter_goldstandard_dict(relations_goldstandard, [FLOW, SAME_GATEWAY])
+    with open("data/other/relations_goldstandard.json", 'w') as file:
+        json.dump(relations_goldstandard, file, indent=4)
+
+    token_goldstandard = load_pickle("data/other/token_goldstandard.pkl")
+    if objects == 'relevant':
+        token_goldstandard = filter_goldstandard_dict(token_goldstandard, [ACTIVITY, XOR_GATEWAY, AND_GATEWAY,
+                                                                           CONDITION_SPECIFICATION])
+    with open("data/other/token_goldstandard.json", 'w') as file:
+        json.dump(token_goldstandard, file, indent=4)
+
+
+if __name__ == '__main__':
+    goldstandards_to_json()
