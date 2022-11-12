90117262084df4319ddc4783c6b08d7f1b3b0a3b
Janek Putz <janekputz@web.de>
2022-11-12 12:00:22 +0100

    training script: echo & eval cmd as string

--- scripts/run_gateway_token_cls.sh
+++ scripts/run_gateway_token_cls.sh
@@ -5,18 +5,23 @@ export CUDA_VISIBLE_DEVICES=1
 
 # conda activate thesis
 
-python ../GatewayTokenClassifier_train.py --batch_size=8 --epochs=3 --ensemble=True --routine=cv --folds=5 --routine=cv \
-            --labels=all --other_labels_weight=0.1 --sampling_strategy=normal
-exit 1
+cmd="python ../GatewayTokenClassifier_train.py --batch_size=8 --epochs=3 --ensemble=True --routine=cv --folds=5 --routine=cv \
+            --labels=all --other_labels_weight=0.1 --sampling_strategy=normal"
+echo $cmd
+eval $cmd
 
 # try all sampling methods with ensembles and best combination of label/weight
 for SAMPLING in "normal" "up" "down" "og"; do
-    python ../GatewayTokenClassifier_train.py --batch_size=8 --epochs=3 --ensemble=True --routine=cv --folds=5 --routine=cv \
-        --labels=all --other_labels_weight=0.1 --sampling_strategy=$SAMPLING
+    cmd="python ../GatewayTokenClassifier_train.py --batch_size=8 --epochs=3 --ensemble=True --routine=cv --folds=5 --routine=cv \
+        --labels=all --other_labels_weight=0.1 --sampling_strategy=$SAMPLING"
+    echo $cmd
+    eval $cmd
 done
 
 # including synonyms (all data; only gateways) with ensembles and best combination of label/weight
 for SAMPLING in "normal" "og"; do
-    python ../GatewayTokenClassifier_train.py --batch_size=8 --epochs=3 --ensemble=True --routine=cv --folds=5 --routine=cv \
-        --labels=all --other_labels_weight=0.1 --sampling_strategy=$SAMPLING --use_synonyms=True
+    cmd="python ../GatewayTokenClassifier_train.py --batch_size=8 --epochs=3 --ensemble=True --routine=cv --folds=5 --routine=cv \
+        --labels=all --other_labels_weight=0.1 --sampling_strategy=$SAMPLING --use_synonyms=True"
+    echo $cmd
+    eval $cmd
 done
