7351b7eaa553868f86f916f7bb6e2a76e2e3f5a6
Janek Putz <janekputz@web.de>
2022-11-24 14:27:22 +0100

    large training update: remove single training files by generalizing cross validation & full training

---
+++ training.py
@@ -0,0 +1,202 @@
+import argparse
+import json
+import logging
+import os
+from typing import List, Tuple, Dict
+
+import tensorflow as tf
+from petreader.labels import XOR_GATEWAY
+
+from Ensemble import Ensemble
+from metrics import f1_normal
+from token_approaches.GatewayTokenClassifier import GatewayTokenClassifier
+from token_approaches.SameGatewayClassifier import SameGatewayClassifier
+from token_approaches.same_gateway_data_preparation import create_same_gateway_cls_dataset_cv
+from utils import get_seed_list
+
+logger = logging.getLogger('Training')
+
+# A) HELPER METHODS & DATA FOR TRAINING AND CROSS VALIDATION
+
+
+# dictionary of metrics for early stopping and cross validation for each model
+MONITOR_METRICS = 'monitor_metric'
+METRICS_PER_FOLD = 'metrics_per_fold'
+
+_model_metrics = {
+    GatewayTokenClassifier: {
+        MONITOR_METRICS: "val_xor_precision",
+        METRICS_PER_FOLD: ['avg_val_loss', 'avg_val_xor_precision', 'avg_val_xor_recall', 'avg_val_xor_f1',
+                           'avg_val_xor_f1_m', 'avg_val_and_recall', 'avg_val_and_precision', 'avg_val_and_f1',
+                           'avg_val_and_f1_m', 'avg_val_overall_accuracy', 'val_loss', 'val_xor_precision',
+                           'val_xor_recall', 'val_xor_f1', 'val_xor_f1_m', 'val_and_recall', 'val_and_precision',
+                           'val_and_f1', 'val_and_f1_m', 'val_overall_accuracy']
+    },
+    SameGatewayClassifier: {
+        MONITOR_METRICS: "val_binary_accuracy",
+        METRICS_PER_FOLD: ['avg_val_loss', 'avg_val_binary_accuracy', 'avg_val_precision', 'avg_val_recall', 'val_loss',
+                           'val_binary_accuracy', 'val_precision', 'val_recall']
+    }
+}
+
+
+def get_empty_metrics_per_fold(model_class: type(tf.keras.Model)) -> Dict:
+    """
+    creates empty dictionary for metrics to monitor for each fold during cross validation of the model
+    :return: dictionary
+    """
+    return {m: 0 if m.startswith("avg") else [] for m in _model_metrics[model_class][METRICS_PER_FOLD]}
+
+
+def get_monitor(model_class: type(tf.keras.Model)) -> str:
+    """
+    return monitor metrics for early stopping for the given model
+    :param model_class: model
+    :return: metric name
+    """
+    return _model_metrics[model_class][MONITOR_METRICS]
+
+
+def compute_avg_metrics(metrics_per_fold):
+    # compute f1 score manually again
+    for i in range(len(metrics_per_fold['val_xor_precision'])):
+        if 'val_xor_f1_m' in metrics_per_fold:
+            metrics_per_fold['val_xor_f1_m'].append(f1_normal(metrics_per_fold['val_xor_precision'][i],
+                                                              metrics_per_fold['val_xor_recall'][i]))
+        if 'val_and_f1_m' in metrics_per_fold:
+            metrics_per_fold['val_and_f1_m'].append(f1_normal(metrics_per_fold['val_and_precision'][i],
+                                                              metrics_per_fold['val_and_recall'][i]))
+
+    # average metrics over folds
+    for metric, value in metrics_per_fold.items():
+        if not metric.startswith("avg_") and not metric.startswith("seed-results-"):
+            metrics_per_fold[f"avg_{metric}"] = round(np.mean(value), 4)
+
+
+def print_metrics(metrics_per_fold):
+    print(' Score per fold '.center(100, '-'))
+    for i in range(len(metrics_per_fold['val_loss'])):
+        if i > 0: print('-' * 100)
+        metric_str = ' - '.join([f"{metric}: {round(value[i], 4)}" for metric, value in metrics_per_fold.items() if
+                                 not metric.startswith("avg_") and not metric.startswith("seed-results-")])
+        print(f"> Fold {i + 1} - {metric_str}")
+    print()
+    print(' Average scores '.center(100, '-'))
+    print(' - '.join([f"{metric}: {round(value, 4)}" for metric, value in metrics_per_fold.items() if
+                      metric.startswith("avg_")]))
+
+
+# TRAINING FUNCTIONS
+
+
+def cross_validation(args: argparse.Namespace, model_class: type(tf.keras.Model),
+                     folded_datasets: List[Tuple[tf.data.Dataset, tf.data.Dataset]], bert_model=None) -> None:
+    """
+    run training of a given model in a cross validation routine
+    averaged results are outputted into logdir
+    :param args: namespace args
+    :param model_class: model class to cross validate
+    :param folded_datasets: list of train/dev datasets of multiple folds to use for cross validation
+    :param bert_model: bert like transformer model as core of models
+    :return:
+    """
+    logger.info(f"Run {args.folds}-fold cross validation")
+
+    os.makedirs(args.logdir, exist_ok=True)
+    args_logdir_original = args.logdir
+
+    metrics_per_fold = get_empty_metrics_per_fold(model_class)
+
+    # perform k-fold CV
+
+    for i in range(args.folds):
+        logger.info(f" Start training of fold {i} ".center(100, '-'))
+
+        # create fold folder
+        if not args.ensemble:
+            args.logdir = f"{args_logdir_original}/{i + 1}"
+            os.makedirs(args.logdir, exist_ok=True)
+
+        train_dataset, dev_dataset = folded_datasets[i][0], folded_datasets[i][1]
+        # a) fit normal models
+        if not args.ensemble:
+            model = model_class(args, bert_model, mode=args.mode, train_size=len(train_dataset))
+            history = model.fit(
+                train_dataset, epochs=args.epochs, validation_data=dev_dataset,
+                callbacks=[tf.keras.callbacks.TensorBoard(args.logdir, update_freq='batch', profile_batch=0),
+                           tf.keras.callbacks.EarlyStopping(monitor=get_monitor(model_class),
+                                                            min_delta=1e-4, patience=2, mode="max",
+                                                            restore_best_weights=True)]
+            )
+            # store model
+            if args.store_weights:
+                model.save_weights(os.path.join(args.logdir, "weights/weights"))
+
+        # b) fit ensemble model (train multiple seeds for current fold)
+        else:
+            ensemble_model = Ensemble(model_class=model_class, seeds=get_seed_list(args.seeds_ensemble),
+                                      args=args, bert_model=bert_model, mode=args.mode, train_size=len(train_dataset))
+            history = ensemble_model.fit(args, train_dataset=train_dataset, dev_dataset=dev_dataset, fold=i)
+
+        # record fold results (record only validation results; drop training metrics)
+        for metric, values in history.history.items():
+            # record validation value of last epoch for each metric
+            if metric.startswith("val_"):
+                # values = values of metric in each epoch (in case of ensemble, values is already averaged over seeds)
+                metrics_per_fold[metric].append(round(values[-1], 4))
+            elif metric.startswith("seed-results-val_"):
+                # values = list of results of metric for every seed (last epoch)
+                metrics_per_fold[f"{metric}-{i}"] = values
+
+    logger.info("Finished CV, average, print and save results")
+    compute_avg_metrics(metrics_per_fold)
+    print_metrics(metrics_per_fold)
+
+    # save metrics
+    with open(os.path.join(args_logdir_original, "cv_metrics.json"), 'w') as file:
+        json.dump(metrics_per_fold, file, indent=4)
+
+
+def full_training(args: argparse.Namespace, model_class: type(tf.keras.Model), dataset: tf.data.Dataset,
+                  bert_model=None) -> None:
+    """
+    run training with full dataset without validation
+    :param args: namespace args
+    :param model_class: model class to cross validate
+    :param dataset: train dataset
+    :param bert_model: bert like transformer model as core of models
+    :return:
+    """
+    logger.info(f"Run full training")
+    args_dir_original = args.logdir
+
+    if not args.ensemble:
+        # train
+        model = SameGatewayClassifier(args, bert_model, train_size=len(dataset))
+        history = model.fit(
+            dataset, epochs=args.epochs,
+            callbacks=[tf.keras.callbacks.TensorBoard(args.logdir, update_freq="batch", profile_batch=0),
+                       tf.keras.callbacks.EarlyStopping(monitor=get_monitor(model_class),
+                                                        min_delta=1e-4, patience=2, mode="max",
+                                                        restore_best_weights=True)]
+        )
+
+        # store model
+        if args.store_weights:
+            model.save_weights(os.path.join(args.logdir, "weights/weights"))
+
+    else:
+        # train
+        args_dir_original = args.logdir
+        ensemble_model = Ensemble(model_class=model_class, seeds=get_seed_list(args.seeds_ensemble),
+                                  args=args, bert_model=bert_model, mode=args.mode, train_size=len(dataset))
+        history = ensemble_model.fit(args, train_dataset=dataset, save_single_models=args.store_weights)
+
+    # store metrics
+    with open(os.path.join(args_dir_original, "metrics.json"), 'w') as file:
+        json.dump(history.history, file, indent=4)
+
+
+if __name__ == '__main__':
+    folded_datasets_sg = create_same_gateway_cls_dataset_cv(XOR_GATEWAY, None, shuffle=True)
+    cross_validation(None, SameGatewayClassifier, folded_datasets_sg)
